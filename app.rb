6b1643797135772f44ace79bbb34315adadbffe3
Grace Shih <vocalise@gmail.com>
2018-07-07 19:12:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/display_puppy' do
+    @breed = params[:breed]
+    @age = params[:age]
+    @name = params[:name]
+    erb :display_puppy
+  end
 
 end

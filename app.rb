0bfe8fef42d1b1df0bdd4f4ece0cd67a18939251
Grace Shih <vocalise@gmail.com>
2018-07-07 19:10:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
   
-  post '/' do
+  post '/display_puppy' do
     @breed = params[:breed]
     @age = params[:age]
     @name = params[:name]
